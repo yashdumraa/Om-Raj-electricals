47147adb285eeddde2f3021cb10bc0d504a8dfb9
yashdumraa <dumra.yash.963@gmail.com>
2026-02-24 19:28:34 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Om Raj Electrical | Best Electrical Shop in Haridwar",
+  description: "Wholesale and Retail Electrical Items in Khanna Nagar Main Road, Haridwar."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
